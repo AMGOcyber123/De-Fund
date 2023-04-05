568f5293cf2454838cc0655b818b679089be26b0
AMGOcyber123 <aayumodi123@gmail.com>
2023-04-05 14:27:31 +0530

    Final Commit (lol)

---
+++ client/src/constants/index.js
@@ -0,0 +1,31 @@
+import { createCampaign, dashboard, logout, payment, profile } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'dashboard',
+    imgUrl: dashboard,
+    link: '/home',
+  },
+  {
+    name: 'campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+  },
+  {
+    name: 'payment',
+    imgUrl: payment,
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'profile',
+    imgUrl: profile,
+    link: '/profile',
+  },
+  {
+    name: 'logout',
+    imgUrl: logout,
+    link: '/',
+    disabled: true,
+  },
+];
